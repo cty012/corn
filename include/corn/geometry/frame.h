0e3e74638d19a8b4cfdd245e487f7b0242e9ad08
cty012 <tianyue.cao21@gmail.com>
2023-10-16 16:23:03 -0500

    Relative frame

--- include/corn/geometry/frame.h
+++ include/corn/geometry/frame.h
@@ -14,12 +14,16 @@ namespace corn {
      *
      * Note that the absolute location may not lie inside the rectangle.
      *
-     * For example,
+     * Examples of relative location:
      * 1. To create a point with distance 40 to the left side and 30 to the bottom, the relative location
      * should be <40, -30, 0, 1>.
      * 2. To create a point 1/3 to the left side and on the top edge, the relative location should be
      * <0, 0, 1.0/3.0, 0>.
      * 3. To create a point at the center, the relative location should be <0, 0, 0.5, 0.5>.
+     *
+     * @see Vec2
+     * @see Vec4
+     * @see RelativeFrame
      */
     struct ReferenceFrame {
         double left;
@@ -27,7 +31,36 @@ namespace corn {
         double right;
         double bottom;
 
-        ReferenceFrame(double left, double top, double right, double bottom);
-        [[nodiscard]] Vec2 map(const Vec4& relLocation) const;
+        /// @brief Converts the relative location into an absolute location.
+        [[nodiscard]] Vec2 map(const Vec4& relativeLocation) const;
+    };
+
+    /**
+     * @class RelativeFrame
+     * @brief A relative frame is a map between reference frames. In fact, it defines the child reference frame relative
+     * to the parent reference frame by specifying the relative location of the top-left and bottom-right corners.
+     *
+     * Suppose the parent reference frame is parentFrame, the top-left corner's relative location is <x1, y1, z1, w1>,
+     * and the bottom-right corner's relative location is <x2, y2, z2, w2>. Then the child's reference frame will be
+     * <l', t', r', b'> where <l', t'> = parentFrame.map(<x1, y1, z1, w1>) and
+     * <r', b'> = parentFrame.map(<x2, y2, z2, w2>).
+     *
+     * Note that the child reference frame may not lie inside the parent reference frame.
+     *
+     * @see Vec4
+     * @see ReferenceFrame
+     */
+    struct RelativeFrame {
+        Vec4 topLeft;
+        Vec4 bottomRight;
+
+        /**
+         * @brief Initialize with the relative location of the top-left and bottom-right corner with respect to the
+         * parent's reference frame.
+         */
+        RelativeFrame(const Vec4& topLeft, const Vec4& bottomRight);
+
+        /// @brief Converts the parent's reference frame into the child's reference frame.
+        [[nodiscard]] ReferenceFrame map(const ReferenceFrame& parentFrame) const;
     };
 }
