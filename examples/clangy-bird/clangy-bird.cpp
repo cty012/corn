10a18afa7be4a089805b3e52e028b0b3cda54d87
cty012 <tianyue.cao21@gmail.com>
2023-11-17 18:33:59 -0600

    Update clangy-bird.cpp

--- examples/clangy-bird/clangy-bird.cpp
+++ examples/clangy-bird/clangy-bird.cpp
@@ -2,12 +2,14 @@
 #include <corn/media.h>
 #include <corn/util.h>
 #include "scenes.h"
+#include "constants.h"
 
 int main() {
     corn::Config config;
     config.title = "Clangy Bird";
     config.width = WIDTH;
     config.height = HEIGHT;
+    config.antialiasing = 4;
     corn::FontManager::instance().load("noto-sans-zh", "fonts/noto-sans-zh/NotoSansSC-VariableFont_wght.ttf");
     corn::Game game(new GameScene(), config);
     game.run();
