1fd9afcd5347f4b391ceb03889e2d1ed0e1119f2
cty012 <tianyue.cao21@gmail.com>
2023-12-18 22:46:27 -0600

    Count number of utf8 chars

--- include/corn/util/utf8.h
+++ include/corn/util/utf8.h
@@ -11,10 +11,17 @@ namespace corn {
     std::u8string unicodeToUTF8(unsigned int codepoint);
 
     /**
+     * @brief Count the number of UTF-8 characters in the string.
+     * @param str The UTF-8 string.
+     * @return The number of UTF-8 characters.
+     */
+    size_t count(const std::u8string& str);
+
+    /**
      * @brief Print a UTF-8 string onto the console.
-     * @param target The UTF-8 string to display.
+     * @param str The UTF-8 string to display.
      *
      * Before calling this function, make sure the console is capable of displaying UTF-8 text.
      */
-    void printu8(const std::u8string& target);
+    void printu8(const std::u8string& str);
 }
