8590edc43a1e244e3ef30c1afb658f64a26d3ffc
cty012 <tianyue.cao21@gmail.com>
2023-12-21 15:09:34 -0600

    continue

--- src/corn/media/text_render_impl.h
+++ src/corn/media/text_render_impl.h
@@ -4,7 +4,6 @@
 #include <utility>
 #include <vector>
 #include <SFML/Graphics.hpp>
-#include <corn/media/text_render.h>
 
 namespace corn {
     class TextRender::TextRenderImpl {
@@ -13,6 +12,8 @@ namespace corn {
             std::vector<std::pair<sf::Text, Color>> contents;
             size_t length;
             Vec2 size;
+
+            Line();
         };
 
         RichText original;
@@ -21,11 +22,38 @@ namespace corn {
         bool limitWidth;
         Vec2 size;
 
+        /// @brief Constructor.
         explicit TextRenderImpl(const RichText& richText);
 
+        /**
+         * @brief Setter for the rich text. Will reset all cached information and recalculate words.
+         * @param richText The new rich text.
+         */
         void setRichText(const RichText& richText);
 
+        /**
+         * @brief Force reset the width of the text.
+         * @param width New width of the text. If negative, width limit will be removed.
+         */
         void setWidth(float width);
+
+    private:
+        /**
+         * @brief Helper function. Inserts a segment of the text into the lines respecting the width limit.
+         * @param segment The segment broken into words.
+         * @param style The style of the segment.
+         * @param width The width to fit in.
+         */
+        void insertSegment(const std::vector<std::u8string>& segment, const TextStyle& style, float width);
+
+        /**
+         * @brief Helper function. Inserts
+         * @param word
+         * @param style
+         * @param width
+         * @return
+         */
+        static const char8_t* insertCharsToEmptyLine(const char8_t* word, Line* line, const TextStyle& style, float width);
     };
 
     void setTextString(sf::Text& text, const std::u8string& str);
