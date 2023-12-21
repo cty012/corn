041dbe64e4bde7ebde6935184237f918501707fd
cty012 <tianyue.cao21@gmail.com>
2023-12-21 15:09:34 -0600

    continue

--- src/corn/media/text_render.cpp
+++ src/corn/media/text_render.cpp
@@ -4,6 +4,8 @@
 #include "text_render_impl.h"
 
 namespace corn {
+    TextRender::TextRenderImpl::Line::Line() : length(0), size() {}
+
     TextRender::TextRenderImpl::TextRenderImpl(const RichText& richText) : lines(), limitWidth(false) {
         this->setRichText(richText);
     }
@@ -24,81 +26,10 @@ namespace corn {
         printf("Limit width: %d\n", this->limitWidth);
 
         this->lines.clear();
-        Line* currentLine = &this->lines.emplace_back();
-        currentLine->length = 0;
-        currentLine->size = Vec2::ZERO();
-
-        // Whether started a new line, the first line doesn't count
-        bool startOfNewLine = false;
 
         // Iterate over all words
         for (const auto& [segment, style] : this->words) {
-            for (const std::u8string& word: segment) {  // todo
-                // Skip empty words (if any)
-                if (word.empty()) {
-                    continue;
-                }
-
-                // If is line feed character, start a new line
-                if (word[0] == '\n') {
-                    currentLine = &this->lines.emplace_back();
-                    currentLine->length = 0;
-                    currentLine->size = Vec2::ZERO();
-                    startOfNewLine = true;
-                    continue;
-                }
-
-                // If start of new line, skip pure space words
-                if (startOfNewLine && isWhitespace(getChar(word))) {
-                    continue;
-                }
-
-                // Measure width
-                sf::Text text;
-                setTextFont(text, style.font);
-                setTextSize(text, (unsigned int) style.size);
-                setTextVariant(text, style.variant);
-                setTextString(text, word);
-                sf::Rect<float> bounds = text.getLocalBounds();
-                Vec2 newSize = {bounds.width, bounds.height};
-
-                // TODO: character wrap on word too long
-                float newWidth = newSize.x + currentLine->size.x;
-                if (currentLine->length == 0 || !this->limitWidth || newWidth <= width) {
-                    // If the word can fit in current line
-                    // Push the word to the back of the line
-                    currentLine->contents.emplace_back(text, style.color);
-                    startOfNewLine = false;
-
-                    // Add to total length & size
-                    currentLine->length += count(word);
-                    currentLine->size.x = newWidth;
-                    currentLine->size.y = std::max(currentLine->size.y, newSize.y);
-                } else {
-                    // If the word cannot fit in current line
-                    // Wrap to next line
-                    currentLine = &this->lines.emplace_back();
-                    currentLine->length = 0;
-                    currentLine->size = Vec2::ZERO();
-                    startOfNewLine = true;
-
-                    // Add word to new line if is not whitespace
-                    if (!isWhitespace(getChar(word))) {
-                        currentLine->contents.emplace_back(text, style.color);
-                        startOfNewLine = false;
-
-                        // Add to total length & size
-                        currentLine->length += count(word);
-                        currentLine->size.x += newSize.x;
-                        currentLine->size.y = std::max(currentLine->size.y, newSize.y);
-                    }
-                }
-            }
-        }
-
-        // Check if the last line is empty
-        if (this->lines.back().length == 0) {
-            this->lines.pop_back();
+            this->insertSegment(segment, style, width);
         }
 
         // Find total size
@@ -176,5 +107,149 @@ namespace corn {
                 text.setStyle(sf::Text::Underlined);
                 break;
         }
+    }
+
+    void TextRender::TextRenderImpl::insertSegment(
+            const std::vector<std::u8string>& segment, const TextStyle& style, float width) {
+
+        // Current line to insert to
+        Line* currentLine = this->lines.empty() ? nullptr : &this->lines.back();
+
+        // Set up SFML buffer text for measuring the width of buffer string.
+        std::u8string bufferString, newBufferString;
+        sf::Text bufferText, newBufferText;
+        setTextFont(bufferText, style.font);
+        setTextSize(bufferText, (unsigned int) style.size);
+        setTextVariant(bufferText, style.variant);
+        setTextString(bufferText, u8"");
+        setTextFont(newBufferText, style.font);
+        setTextSize(newBufferText, (unsigned int) style.size);
+        setTextVariant(newBufferText, style.variant);
+
+        // Iterate over all words
+        for (size_t i = 0; i < segment.size(); i++) {
+            const std::u8string& word = segment[i];
+
+            // Skip empty words (if any)
+            if (word.empty()) continue;
+
+            // If is line feed character
+            if (word[0] == '\n') {
+                // First push the current buffer to end of line
+                if (currentLine && !bufferString.empty()) {
+                    currentLine->contents.emplace_back(bufferText, style.color);
+                }
+                // Then start a new line
+                currentLine = &this->lines.emplace_back();
+                continue;
+            }
+
+            // If start of new line, skip pure space words
+            bool notFirstLine = this->lines.size() > 1;
+            bool currentLineEmpty = currentLine && currentLine->length == 0 && bufferString.empty();
+            if (notFirstLine && currentLineEmpty && isWhitespace(getChar(word))) {
+                continue;
+            }
+
+            // Measure width and height
+            newBufferString = bufferString + word;
+            setTextString(newBufferText, newBufferString);
+            sf::Rect<float> newBounds = newBufferText.getLocalBounds();
+            Vec2 newSize = { newBounds.width, newBounds.height };
+
+            // Check if the word can fit
+            if (currentLine && (!this->limitWidth || newSize.x + currentLine->size.x <= width)) {
+                // 1. If the word can fit in current line
+                // Push the new text to the back of the line
+                currentLine->contents.emplace_back(newBufferText, style.color);
+                // Add to total length & size
+                currentLine->length += count(newBufferString);
+                currentLine->size.x = newSize.x + currentLine->size.x;
+                currentLine->size.y = std::max(currentLine->size.y, newSize.y);
+                // Update buffer
+                bufferString = std::move(newBufferString);
+                bufferText = std::move(newBufferText);
+            } else if (currentLineEmpty) {
+                // 2. If the word cannot fit, but current line is empty
+                const char8_t* start = word.c_str();
+                while (true) {
+                    start = corn::TextRender::TextRenderImpl::insertCharsToEmptyLine(start, currentLine, style, width);
+                    if (!start) break;
+                    // Wrap to next line
+                    currentLine = &this->lines.emplace_back();
+                }
+                // Put the remaining characters in the buffer
+                bufferString = std::u8string(start);
+                setTextString(bufferText, bufferString);
+
+            } else {
+                // 3. Otherwise, push the original text in current line and change line. Do not move forward.
+                // Push original text to the back of the line
+                if (currentLine && !bufferString.empty()) {
+                    currentLine->contents.emplace_back(bufferText, style.color);
+                    // Add to total length & size
+                    currentLine->length += count(bufferString);
+                    currentLine->size.x = newSize.x + currentLine->size.x;
+                    currentLine->size.y = std::max(currentLine->size.y, newSize.y);
+                }
+                // Wrap to next line
+                currentLine = &this->lines.emplace_back();
+                // Update buffer
+                bufferString.clear();
+                bufferText = sf::Text();
+                // Do not move forward
+                i--;
+            }
+        }
+
+        // If buffer not empty, push the buffer to the back of the current line
+        if (currentLine && !bufferString.empty()) {
+            currentLine->contents.emplace_back(bufferText, style.color);
+        }
+    }
+
+    const char8_t* TextRender::TextRenderImpl::insertCharsToEmptyLine(
+            const char8_t* word, TextRender::TextRenderImpl::Line* line, const TextStyle& style, float width) {
+
+        // Set up SFML buffer text for measuring the width of buffer string.
+        std::u8string bufferString, newBufferString;
+        sf::Text bufferText, newBufferText;
+        setTextFont(bufferText, style.font);
+        setTextSize(bufferText, (unsigned int) style.size);
+        setTextVariant(bufferText, style.variant);
+        setTextString(bufferText, u8"");
+        setTextFont(newBufferText, style.font);
+        setTextSize(newBufferText, (unsigned int) style.size);
+        setTextVariant(newBufferText, style.variant);
+
+        while (*word) {
+            // Get a UTF-8 character from the remaining word
+            std::u8string nextChar = getChar(word);
+            newBufferString += nextChar;
+
+            // Measure width and height
+            setTextString(newBufferText, newBufferString);
+            sf::Rect<float> newBounds = newBufferText.getLocalBounds();
+            Vec2 newSize = { newBounds.width, newBounds.height };
+
+            if (newSize.x <= width) {
+                // If new character can fit
+                bufferString = std::move(newBufferString);
+                bufferText = std::move(newBufferText);
+                word += nextChar.size();
+            } else {
+                // If new character cannot fit
+                // Push original text to the back of the line
+                if (line && !bufferString.empty()) {
+                    line->contents.emplace_back(bufferText, style.color);
+                    // Set total length & size
+                    line->length = count(bufferString);
+                    line->size = newSize;
+                }
+                return word;
+            }
+        }
+
+        return nullptr;
     }
 }
