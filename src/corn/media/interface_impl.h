52fd583791aa64090c7513cd1d78f0d5c9d26800
cty012 <tc37@illinois.edu>
2025-04-02 19:28:54 -0500

    CMake

--- src/corn/media/interface_impl.h
+++ src/corn/media/interface_impl.h
@@ -1,13 +1,6 @@
 #pragma once
 
 #include <bgfx/bgfx.h>
-#if BX_PLATFORM_LINUX
-#define GLFW_EXPOSE_NATIVE_X11
-#elif BX_PLATFORM_WINDOWS
-#define GLFW_EXPOSE_NATIVE_WIN32
-#elif BX_PLATFORM_OSX
-#define GLFW_EXPOSE_NATIVE_COCOA
-#endif
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
 #include "../render/shader.h"
